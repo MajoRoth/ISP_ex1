a183f1761909290dd22c8701b58b9b4ca39ac7fa
amitroth <amitroth@gmail.com>
2023-04-19 22:28:27 +0300

    b and c

--- playground.py
+++ playground.py
@@ -1,3 +1,6 @@
+import torch
+import torchaudio
+
 
 def part1():
     import matplotlib.pyplot as plt
@@ -68,7 +71,25 @@ def part2():
     plt.legend(loc="best")
 
 
-    # DFT - TODO
+    fft1 = np.fft.fft(sin1)
+    fft2 = np.fft.fft(sin2)
+    fft3 = np.fft.fft(sin2+sin1)
+    freqs = np.fft.fftfreq(len(sin1))
+
+    fig, ax = plt.subplots(3, 1, sharex=True)
+
+    len_to_cut = int(len(sin1)/2)
+
+    ax[0].plot(freqs[len_to_cut:], abs(fft1)[len_to_cut:])
+    ax[0].set_ylabel('|FT(sin1)|')
+
+    ax[1].plot(freqs[len_to_cut:], abs(fft2)[len_to_cut:])
+    ax[1].set_ylabel('|FT(sin2)|')
+
+    ax[2].plot(freqs[len_to_cut:], abs(fft3)[len_to_cut:])
+    ax[2].set_xlabel('Frequency')
+    ax[2].set_ylabel('|FT(sin2 + sin3)|')
+    plt.show()
 
     # dft1 = DFT(sin1)
     # plt.figure()
@@ -77,7 +98,122 @@ def part2():
 
     plt.show()
 
+def part3():
+    """
+    Part C: playing with sampling rate.
+
+    In this part we would observe the influences of sampling frequency by using a toy setup where we control the sampling frequency of a given cascaded sine wave.
+
+    1. Generate a sine wave corresponding to `sum_{i=0 to 4}(sine(2^{i} * 1000 [Hz]))` using a 32[KHz] frequency, with duration of 3 secs.
+    2. Define a function `resample(signal, new_fs, old_fs)` that downsamples the given signal from old_fs frequency to new_fs frequency.
+       Use `torchaudio.transforms.Resample` for this purpose.
+    3. Downsamle the generated signal to 16[KHz] and to 8[KHz] by by 'skipping' samples, i.e. downsampling 32Khz to 16KHz is simply taking every other sample..
+    4. Use `torchaudio.transforms.Resample` to artificially resume 32[KHz] frequency for the downsampled signals
+       (this is to ensure that your fft plots would contain spectral frequencies ranging from 0-16k, why is that?).
+    5. Observe the FFT plots of the generated signals, do you see any phenomena occuring? describe the phenomena.
+       why is this happening? explain. Hint: what happens at 2[KHz], 4[KHz]? is it different for other frequencies?
+
+    Note: your FFT plots should plot all frequencies ranging 0 - 16000
+    :return:
+    """
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    def sine_by_i(duration, i):
+        return np.sin(duration * 2 * np.pi* 2**i * 1000)
+
+    def resample(signal, new_fs, old_fs):
+        signal_tensor = torch.from_numpy(signal).unsqueeze(0)
+        resample_transform = torchaudio.transforms.Resample(old_fs, new_fs)
+        resample_transform.type('torch.DoubleTensor')
+        downsampled_signal_tensor = resample_transform(signal_tensor)
+        downsampled_signal = downsampled_signal_tensor.numpy()[0]
+        return downsampled_signal
+
+    seconds = 3
+    sr = 32000
+    sr16 = 16000
+    sr8 = 8000
+    N_SAMPLES_TO_PLOT = 200
+    t = np.linspace(0, seconds, seconds * sr)
+
+    sin_arrays = list()
+    for i in range(5):
+        sin_arrays.append(sine_by_i(t, i))
+    sin_sum = np.sum(sin_arrays, axis=0)
+
+
+    unsampled16 = resample(sin_sum, sr16, sr)
+    time16 = np.linspace(0, seconds, seconds * sr16)
+
+    unsampled8 = resample(sin_sum, sr8, sr)
+    time8 = np.linspace(0, seconds, seconds * sr8)
+
+    fig, ax = plt.subplots(3, 1)
+
+    ax[0].plot(t[:N_SAMPLES_TO_PLOT], sin_sum[:N_SAMPLES_TO_PLOT])
+    ax[0].set_title("down sampling")
+    ax[0].set_ylabel('32 Khz')
+
+    ax[1].plot(time16[:N_SAMPLES_TO_PLOT//2], unsampled16[:N_SAMPLES_TO_PLOT//2])
+    ax[1].set_ylabel('16 Khz')
+
+    ax[2].plot(time8[:N_SAMPLES_TO_PLOT//4], unsampled8[:N_SAMPLES_TO_PLOT//4])
+    ax[2].set_xlabel('time')
+    ax[2].set_ylabel('8 Khz')
+
+    plt.show()
+
+
+    upsampled16 = resample(unsampled16, sr, sr16)
+    upsampled8 = resample(unsampled8, sr, sr8)
+
+    fig, ax = plt.subplots(3, 1)
+    ax[0].set_title("up sampling")
+
+    ax[0].plot(t[:N_SAMPLES_TO_PLOT], sin_sum[:N_SAMPLES_TO_PLOT])
+    ax[0].set_ylabel('32 Khz')
+
+    ax[1].plot(t[:N_SAMPLES_TO_PLOT], upsampled16[:N_SAMPLES_TO_PLOT])
+    ax[1].set_ylabel('32 Khz')
+
+    ax[2].plot(t[:N_SAMPLES_TO_PLOT], upsampled8[:N_SAMPLES_TO_PLOT])
+    ax[2].set_xlabel('time')
+    ax[2].set_ylabel('32 Khz')
+
+    plt.show()
+
+    fft1 = np.fft.fft(sin_sum)
+    fft2 = np.fft.fft(upsampled16)
+    fft3 = np.fft.fft(upsampled8)
+    freqs = np.fft.fftfreq(len(fft1))
+
+    fig, ax = plt.subplots(3, 1, sharex=True)
+
+    ax[0].set_title("Fourier of up sampled waveforms")
+
+    len_to_cut = int(len(fft1) / 2)
+
+    ax[0].plot(freqs[:len_to_cut], abs(fft1)[:len_to_cut])
+    ax[0].set_ylabel('original')
+
+    ax[1].plot(freqs[:len_to_cut], abs(fft2)[:len_to_cut])
+    ax[1].set_ylabel('16 Khz')
+
+    ax[2].plot(freqs[:len_to_cut], abs(fft3)[:len_to_cut])
+    ax[2].set_xlabel('Frequency (Hz)')
+    ax[2].set_ylabel('8 khz')
+    plt.show()
+
+    """
+        We see the implications of Nyquist theorem.
+        only frequencies which are smaller then 0.5 * sr could be restored.
+        hence the 2Khz and 4Khz are seen in every sample, because the smallest sampling rate is 8Khz
+        but the bigger frequencies disappear.
+    """
+
+
 
 
 if __name__ == "__main__":
-    part2()
+    part3()
