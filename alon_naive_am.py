31e0956ae99f2ff77d16ece73ca588aebee7f745
alonzi <alonzi@cs.huji.ac.il>
2023-04-23 17:26:59 +0300

    naive audio manipulation - part B

--- alon_naive_am.py
+++ alon_naive_am.py
@@ -43,7 +43,53 @@ def partB():
     Note: This should be a Naive implementation, achieveable using torch.stft, torch.istft, torch.fft.fft, torch.fft.ifft alone and programable in a few lines.
     """
 
+    import torch
+    import torchaudio
+    import os
+
+
+    def create_if_not_exists(dir):
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+
+    def naive_time_stretch(y, scale_factor):
+        WINDOW_SIZE = 1024
+        y_stft = torch.stft(y, n_fft=WINDOW_SIZE)
+        ch, n_freqs, n_windows, _ = y_stft.shape
+
+        step = 1 / scale_factor
+        soft_win_idx = 0.0
+        stretched_stft = None
+        k = 0
+        while(k < n_windows):
+            # jump by step, and concat original stft windows (downsampe/duplicate depending on scale_factor)
+            win_fft = y_stft[:, :, k, :].unsqueeze(2)
+            if stretched_stft is None:
+                stretched_stft = win_fft
+            else:
+                stretched_stft = torch.concat((stretched_stft, win_fft), dim=2)
+            soft_win_idx += step
+            k = int(round(soft_win_idx))
+
+        return torch.istft(stretched_stft, n_fft=WINDOW_SIZE)
+
+
+    # create outputs dir
+    OUTPUTS_DIR = "outputs"
+    create_if_not_exists(OUTPUTS_DIR)
+    y, sr = torchaudio.load("audio_16k/Basta_16k.wav")
+    SCALES = [0.8, 1.2]
+
+    for stretch_scale in SCALES:
+        y_stretched = naive_time_stretch(y, scale_factor=stretch_scale)
+        outfile = f"{OUTPUTS_DIR}/naive_pitch_shift_{str(stretch_scale).replace('.', '_')}.wav"
+        print(f"saving outfile: {outfile}")
+        torchaudio.save(outfile, y_stretched, sr)
+
+
+
 if __name__ == "__main__":
-    partA()
-    # partB()
+    # partA()
+    partB()
     pass
